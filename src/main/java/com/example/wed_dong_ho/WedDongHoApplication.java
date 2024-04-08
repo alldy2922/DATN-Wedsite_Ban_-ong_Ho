8cd64fcf61e6624a72d5cde4c5367820b1e3508c
DESKTOP-LR46J8D\LENOVO <thanhtqph30275@fpt.edu.vn>
2024-04-08 12:59:38 +0700

    first commit

---
+++ src/main/java/com/example/wed_dong_ho/WedDongHoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wed_dong_ho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedDongHoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WedDongHoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wed_dong_ho/WedDongHoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wed_dong_ho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedDongHoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
